f5a221e012974210e47d576d66a70cedadc87134
mojo <disobay@proton.me>
2023-04-18 21:17:34 +0000

    change#	modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,6 @@
 import AppForm from "../components/AppForm";
 import AppPreviewContract from "../components/AppPreviewContract";
 
-import styles from '../styles/ConnectWalletButton.module.css';
-
-const Home = () => {
-  return (
-    <div className={styles.container}>
-      <ConnectWalletButton />
-      {/* The rest of your app's UI */}
-    </div>
-  );
-};
-
-export default Home;
-
 export default function Home() {
   return (
     <div>
