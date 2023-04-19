f9e09c9672b5c62cbf50c0aa653ece39fca3fda7
mojo <disobay@proton.me>
2023-04-19 20:37:09 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 import AppForm from "../components/AppForm";
 import AppPreviewContract from "../components/AppPreviewContract";
+import RemixButton from '../components/RemixButton';
+import styles from '../styles/Home.module.css';
 
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <RemixButton />
+      {/* The rest of your app's UI */}
+    </div>
+  );
+}
 export default function Home() {
   return (
     <div>
